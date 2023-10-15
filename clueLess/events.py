92a091f69ab48b6f744a1ce94daf82bdc2a8b6cd
cmf2196 <cmf2196@columbia.edu>
2023-10-15 15:34:14 -0400

    enforced a player cannot enter empty user name, and enabled player switching characters

--- clueLess/events.py
+++ clueLess/events.py
@@ -17,13 +17,14 @@ characters = {}
 # This connection function is from the tutorial: https://www.youtube.com/watch?v=AMp6hlA8xKA
 @socketio.on("connect")
 def handle_connect():
-    print("Client connected!")
+     print("client connected")
 
 # This adds a player to the player selection view
+# note that the game.html enforces that the user name is nonempty
 @socketio.on("user_join")
 def handle_user_join(username):
-    print(f"User {username} joined!")
     users[username] = request.sid
+    emit("player_joined", broadcast=True)
 
     # if a new player joins, we need to refresh their screen with all the users that have 
     # previously made selections
@@ -31,19 +32,29 @@ def handle_user_join(username):
         emit("playerChoice", {"player":characters[user], "username": user}, broadcast=True)
 
 
+
 # This handles the event where a player selectss a character to use
 @socketio.on("player_select")
 def handle_player_select(playerName):
     username = None 
+
+    # find the username, this is a bad way to go about it
     for user in users:
         if users[user] == request.sid:
             username = user  
-            characters[user] = playerName      
-    emit("playerChoice", {"player":playerName, "username": username}, broadcast=True)
+            dictValues = characters.values()
+            dictUsers  = characters.keys()
+            # check to make sure another player hasnt chosen this character
+            if playerName not in dictValues:
+                # if a player is switching their character
+                if user in dictUsers:
+                    oldChar = characters[user]
+                    emit("playerChoice", {"player":oldChar, "username": ""}, broadcast=True)
 
+                characters[user] = playerName      
+                emit("playerChoice", {"player":playerName, "username": username}, broadcast=True)
 
 # This function starts the game!
 @socketio.on("game_start")
 def handle_game_start():
-    print('game starting')
     emit("start_game", broadcast=True)
