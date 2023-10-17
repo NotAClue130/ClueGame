d37f42498e1339299ee3740309a45e62e4711018
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2023-10-17 23:51:53 +0800

    Add database related and do some rename work
    1. Database related work
    events_sql.py is a revised version of events.py to base events on database. sql.py is auxiliary for it.
    2. Rename work
    variables in events.py are renamed to make it more normalized

--- clueLess/events_sql.py
+++ clueLess/events_sql.py
@@ -5,16 +5,19 @@
 from flask import request
 from flask_socketio import emit
 
-# we will add all the events onto this object, then import 
+# we will add all the events onto this object, then import
 # this in init
 from .extensions import socketio
 
-# This will maintain the users that are in the game. as well as the characters they 
+# This will base the events on database
+from sql import *
+import pymysql
+db = pymysql.connect(host='localhost', port=3306, user='root', password='password', db='database', charset='utf8')
+
+# This will maintain the users that are in the game. as well as the characters they
 # selected.  We can move these to a relational database per Yang's requirements spec.
-dict_sids = {}
-dict_characters = {}
-# The key of both dict sids and dict characters are username.
-# The value of dict sids is sid while the value of characters is character.
+create_table_users(db)
+create_table_characters(db)
 
 # This connection function is from the tutorial: https://www.youtube.com/watch?v=AMp6hlA8xKA
 @socketio.on("connect")
@@ -25,29 +28,25 @@ def handle_connect():
 # note that the game.html enforces that the user name is nonempty
 @socketio.on("user_join")
 def handle_user_join(username):
-    dict_sids[username] = request.sid
+    insert_table_users(db, username, request.sid)
     emit("player_joined", broadcast=True)
 
-    # if a new player joins, we need to refresh their screen with all the users that have 
+    # if a new player joins, we need to refresh their screen with all the users that have
     # previously made selections
-    for username_i in dict_characters:
-        emit("playerChoice", {"player":dict_characters[username_i], "username": username_i}, broadcast=True)
+    for character, username in query_table_characters(db):
+        emit("playerChoice", {"player": character, "username": username}, broadcast=True)
 
 
 
 # This handles the event where a player selectss a character to use
 @socketio.on("player_select")
 def handle_player_select(character):
-
+    # "playerName" is synonymous with "character"
     # find the username, this is a bad way to go about it
-    username = None
-    for username_i in dict_sids:
-        if dict_sids[username_i] == request.sid:
-            username = username_i
-            break
-
-    characters = dict_characters.values()
-    usernames = dict_characters.keys()
+    username = query_table_users_on_sid(request.sid)
+    character_username_list = query_table_characters(db)
+    characters = [i[0] for i in character_username_list]
+    usernames = [i[1] for i in character_username_list]
 
     # check to make sure another player hasnt chosen this character
     if character in characters:
@@ -55,10 +54,11 @@ def handle_player_select(character):
 
     # if a player is switching their character
     if username in usernames:
-        character_old = dict_characters[username]
-        emit("playerChoice", {"player": character_old, "username": ""}, broadcast=True)
+        oldCharacter = query_table_characters_on_username(db, username)
+        delete_table_characters_on_username(db, username)
+        emit("playerChoice", {"player":oldCharacter, "username": ""}, broadcast=True)
 
-    dict_characters[username] = character
+    insert_table_characters(character, username)
     emit("playerChoice", {"player": character, "username": username}, broadcast=True)
 
 # This function starts the game!
